68c05f4edf0ee901e02682af533f8dc90fae1953
aaditya <92213034+mindlesssapien@users.noreply.github.com>
2026-06-26 19:17:50 +0530

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,26 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
-end
-
 set -g fish_greeting
 
 set PATH "$HOME/.local/bin:$PATH"
 
+# Set up fzf key bindings
+fzf --fish | source
+
 # Environment variables
 set -gx EDITOR nvim
 set -gx HSA_OVERRIDE_GFX_VERSION 10.3.0
+#set <SHIFT-TAB>
 
 # Use uv tools installed in ~/.local/bin
 fish_add_path ~/.local/bin
 
 alias v="nvim"
+alias ffv='nvim $(fzf -m --preview="bat -n {}")'
+alias tmux='tmux -u'
+
+# opencode
+fish_add_path /home/sapien/.opencode/bin
+
+end
+
